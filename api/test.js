b908d1264d13fa2f403fa4a4dc4b216223fc8a13
drkamel17 <drkamel17@gmail.com>
2026-04-10 00:50:41 +0200

    t

--- api/test.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(request) {
-  return new Response(JSON.stringify({
-    status: 'OK',
-    message: 'API is working',
-    timestamp: new Date().toISOString()
-  }), {
-    headers: { 'Content-Type': 'application/json' }
-  });
-}
